723dca800611e3aeab9ba70ce5d708fdad2ebc32
Michal-Szmuksta-AGH <81320570+Michal-Szmuksta-AGH@users.noreply.github.com>
2022-10-27 11:33:32 +0200

    Add files via upload

---
+++ L4_Michał_Szmuksta/lab3_regexp-Michal-Szmuksta-AGH/main.py
@@ -0,0 +1,100 @@
+
+import numpy as np
+import pickle
+
+import psycopg2 as pg
+import pandas.io.sql as psql
+import pandas as pd
+
+from typing import Union, List, Tuple
+
+from sqlalchemy import create_engine
+
+connection = pg.connect(host='pgsql-196447.vipserv.org', port=5432, dbname='wbauer_adb', user='wbauer_adb', password='adb2020');
+# db_string = "postgresql://wbauer_adb:adb2020@pgsql-196447.vipserv.org:5432/wbauer_adb"
+# db = create_engine(db_string)
+# connection = db.connect()
+
+def film_in_category(category:Union[int,str])->pd.DataFrame:
+    ''' Funkcja zwracająca wynik zapytania do bazy o tytuł filmu, język, oraz kategorię dla zadanego:
+        - id: jeżeli categry jest int
+        - name: jeżeli category jest str, dokładnie taki jak podana wartość
+    Przykład wynikowej tabeli:
+    |   |title          |languge    |category|
+    |0	|Amadeus Holy	|English	|Action|
+    
+    Tabela wynikowa ma być posortowana po tylule filmu i języku.
+    
+    Jeżeli warunki wejściowe nie są spełnione to funkcja powinna zwracać wartość None.
+    
+    Parameters:
+    category (int,str): wartość kategorii po id (jeżeli typ int) lub nazwie (jeżeli typ str)  dla którego wykonujemy zapytanie
+    
+    Returns:
+    pd.DataFrame: DataFrame zawierający wyniki zapytania
+    '''
+    if isinstance(category, int):
+        db = pd.read_sql("SELECT title, la.name, category FROM film WHERE film_id = {} ORDER BY title, category".format(category), con=connection)
+        return db
+    elif isinstance(category, str):
+        db = pd.read_sql("SELECT title, language, category FROM film WHERE title = '{}' ORDER BY title, category".format(category), con=connection)
+        return db
+    else:
+        return None
+
+    
+def film_in_category_case_insensitive(category:Union[int,str])->pd.DataFrame:
+    ''' Funkcja zwracająca wynik zapytania do bazy o tytuł filmu, język, oraz kategorię dla zadanego:
+        - id: jeżeli categry jest int
+        - name: jeżeli category jest str
+    Przykład wynikowej tabeli:
+    |   |title          |languge    |category|
+    |0	|Amadeus Holy	|English	|Action|
+    
+    Tabela wynikowa ma być posortowana po tylule filmu i języku.
+    
+    Jeżeli warunki wejściowe nie są spełnione to funkcja powinna zwracać wartość None.
+    
+    Parameters:
+    category (int,str): wartość kategorii po id (jeżeli typ int) lub nazwie (jeżeli typ str)  dla którego wykonujemy zapytanie
+    
+    Returns:
+    pd.DataFrame: DataFrame zawierający wyniki zapytania
+    '''
+    return None
+    
+def film_cast(title:str)->pd.DataFrame:
+    ''' Funkcja zwracająca wynik zapytania do bazy o obsadę filmu o dokładnie zadanym tytule.
+    Przykład wynikowej tabeli:
+    |   |first_name |last_name  |
+    |0	|Greg       |Chaplin    | 
+    
+    Tabela wynikowa ma być posortowana po nazwisku i imieniu klienta.
+    Jeżeli warunki wejściowe nie są spełnione to funkcja powinna zwracać wartość None.
+        
+    Parameters:
+    title (int): wartość id kategorii dla którego wykonujemy zapytanie
+    
+    Returns:
+    pd.DataFrame: DataFrame zawierający wyniki zapytania
+    '''
+    return None
+    
+
+def film_title_case_insensitive(words:list) :
+    ''' Funkcja zwracająca wynik zapytania do bazy o tytuły filmów zawierających conajmniej jedno z podanych słów z listy words.
+    Przykład wynikowej tabeli:
+    |   |title              |
+    |0	|Crystal Breaking 	| 
+    
+    Tabela wynikowa ma być posortowana po nazwisku i imieniu klienta.
+
+    Jeżeli warunki wejściowe nie są spełnione to funkcja powinna zwracać wartość None.
+        
+    Parameters:
+    words(list): wartość minimalnej długości filmu
+    
+    Returns:
+    pd.DataFrame: DataFrame zawierający wyniki zapytania
+    '''
+    return None
